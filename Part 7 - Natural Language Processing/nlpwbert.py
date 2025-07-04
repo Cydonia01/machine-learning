f7bc980411eb75eaabc11f11f83f0f631a089719
Cydonia01 <malizdmr011@gmail.com>
2025-07-04 21:58:13 +0300

    implemented nlp with bert

---
+++ Part 7 - Natural Language Processing/nlpwbert.py
@@ -0,0 +1,26 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Suppress TensorFlow warnings
+os.environ['TF_USE_LEGACY_KERAS'] = 'True'  # Use legacy Keras for compatibility
+
+import numpy as np
+import tensorflow as tf
+import ktrain
+from ktrain import text
+
+# Creating the training and test sets
+(x_train, y_train), (x_test, y_test), preproc = text.texts_from_folder(datadir="aclImdb", classes=['pos', 'neg'], maxlen=500, train_test_names=['train', 'test'], preprocess_mode='bert')
+print("Number of training samples:", len(x_train))
+print("Number of test samples:", len(x_test))
+
+# Building the BERT model
+model = text.text_classifier(name='bert', train_data=(x_train, y_train), preproc=preproc)
+
+# Training the model
+learner = ktrain.get_learner(model=model, train_data=(x_train, y_train), val_data=(x_test, y_test), batch_size=6)
+learner.fit_onecycle(lr=2e-5, epochs=1)
+
+learner.validate(val_data=(x_test, y_test), class_names=preproc.get_classes())
+# Saving the model
+predictor = ktrain.get_predictor(learner.model, preproc)
+predictor.save("bert_imdb_predictor")
+
