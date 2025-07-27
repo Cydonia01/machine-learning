29cc108f61a7f4485a31f953cca02b799420796b
Cydonia01 <malizdmr011@gmail.com>
2025-07-27 03:18:09 +0300

    implemented pacman with Deep Convolutional RL

---
+++ Part 6 - Reinforcement Learning/Deep Convolutional Q-Learning/pacman.py
@@ -0,0 +1,192 @@
+from collections import deque
+import os
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.autograd as autograd
+from torch.autograd import Variable
+from torch.utils.data import DataLoader, Dataset
+import gymnasium as gym
+import glob
+import io
+import base64
+import imageio
+from IPython.display import HTML, display
+from gymnasium.wrappers import RecordVideo
+import webbrowser
+from gymnasium.envs.registration import registry
+from ale_py import ALEInterface
+
+ale = ALEInterface()
+ale.loadROM("ms_pacman.bin")
+
+# Building the AI
+# Creating the architecture of the neural network
+class Network(nn.Module):
+    def __init__(self, action_size, seed = 42):
+        super(Network, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size = 8, stride = 4)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size = 4, stride = 2)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size = 3, stride = 1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 128, kernel_size = 3, stride = 1)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.fc1 = nn.Linear(128 * 10 * 10, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, action_size)
+
+    def forward(self, state):
+        x = F.relu(self.bn1(self.conv1(state)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+
+# Training the AI
+# Setting up the environment
+env = gym.make('MsPacman-v4', full_action_space = False)
+state_shape = env.observation_space.shape
+state_size = state_shape[0]
+number_actions = env.action_space.n
+
+# Initializing the hyperparameters
+learning_rate = 5e-4
+minibatch_size = 64
+discount_factor = 0.99
+
+# Preprocessing the frames
+from PIL import Image
+from torchvision import transforms
+
+def preprocess_frame(frame):
+    frame = Image.fromarray(frame)
+    preprocess = transforms.Compose([
+        transforms.Resize((128, 128)),
+        transforms.ToTensor(),
+    ])
+    return preprocess(frame).unsqueeze(0)
+
+# Implementing the DQN class
+class Agent():
+    def __init__(self, action_size):
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.action_size = action_size
+        self.local_qnetwork = Network(action_size).to(self.device)
+        self.target_qnetwork = Network(action_size).to(self.device)
+        self.optimizer = optim.Adam(self.local_qnetwork.parameters(), lr = learning_rate)
+        self.memory = deque(maxlen = 10000)
+    
+    def step(self, state, action, reward, next_state, done):
+        state = preprocess_frame(state).cpu()
+        next_state = preprocess_frame(next_state).cpu()
+        self.memory.append((state, action, reward, next_state, done))
+        if len(self.memory) > minibatch_size:
+            experiences = random.sample(self.memory, minibatch_size)
+            self.learn(experiences, discount_factor)
+        
+    def act(self, state, epsilon = 0.):
+        state = preprocess_frame(state).to(self.device)
+        self.local_qnetwork.eval()
+        with torch.no_grad():
+            action_values = self.local_qnetwork(state)
+        self.local_qnetwork.train()
+        if random.random() > epsilon:
+            return np.argmax(action_values.cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.action_size))
+    
+    def learn(self, experiences, discount_factor):
+        states, actions, rewards, next_states, dones = zip(*experiences)
+        states = torch.cat(states).float().to(self.device)
+        actions = torch.from_numpy(np.vstack(actions)).long().to(self.device)
+        rewards = torch.from_numpy(np.vstack(rewards)).float().to(self.device)
+        next_states = torch.cat(next_states).float().to(self.device)
+        dones = torch.from_numpy(np.vstack(dones).astype(np.uint8)).float().to(self.device)
+        next_q_targets = self.target_qnetwork(next_states).detach().max(1)[0].unsqueeze(1)
+        q_targets = rewards + (discount_factor * next_q_targets * (1 - dones))
+        q_expected = self.local_qnetwork(states).gather(1, actions)
+        loss = F.mse_loss(q_expected, q_targets)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+            
+# Initializing the agent
+agent = Agent(number_actions)
+
+# Training the agent
+number_episodes = 2000
+maximum_number_timesteps_per_episode = 10000
+epsilon_starting_value = 1.0
+epsilon_ending_value = 0.01
+epsilon_decay_value = 0.995
+epsilon = epsilon_starting_value
+scores_on_100_episodes = deque(maxlen = 100)
+
+record_training = True
+record_every_n_episodes = 50
+all_training_frames = []
+
+for episode in range(1, number_episodes + 1):
+    env = gym.make('MsPacman-v4', render_mode='rgb_array')
+    state, _ = env.reset()
+    score = 0
+    episode_frames = []
+    
+    for t in range(maximum_number_timesteps_per_episode):
+        if record_training and episode % record_every_n_episodes == 0:
+            episode_frames.append(env.render())
+        
+        action = agent.act(state, epsilon)
+        next_state, reward, done, _, _ = env.step(action)
+        agent.step(state, action, reward, next_state, done)
+        state = next_state
+        score += reward
+        if done:
+            break
+        
+    if record_training and episode % record_every_n_episodes == 0:
+        all_training_frames.extend(episode_frames)
+        
+    scores_on_100_episodes.append(score)
+    epsilon = max(epsilon_ending_value, epsilon_decay_value * epsilon)
+    print('\rEpisode {}\tAverage Score: {:.2f}\t'.format(episode, np.mean(scores_on_100_episodes)), end="")
+    
+    if episode % 100 == 0:
+        print('\rEpisode {}\tAverage Score: {:.2f}\t'.format(episode, np.mean(scores_on_100_episodes)))
+    
+    if np.mean(scores_on_100_episodes) >= 500.0:
+        print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(episode - 100, np.mean(scores_on_100_episodes)))
+        torch.save(agent.local_qnetwork.state_dict(), 'checkpoint.pth')
+        break
+
+if record_training and all_training_frames:
+    imageio.mimsave('training_video.mp4', all_training_frames, fps=90)
+    webbrowser.open('training_video.mp4')
+
+# agent.local_qnetwork.load_state_dict(torch.load('checkpoint.pth', map_location=agent.device))
+    
+# Visualizing the results
+def show_video_of_model(agent, env_name):
+    env = gym.make(env_name, render_mode='rgb_array')
+    state, _ = env.reset()
+    done = False
+    frames = []
+    while not done:
+        frame = env.render()
+        frames.append(frame)
+        action = agent.act(state)
+        state, _, done, _, _ = env.step(action.item())
+    env.close()
+    imageio.mimsave('video.mp4', frames, fps=30)
+    
+show_video_of_model(agent, 'MsPacman-v4')
+webbrowser.open('video.mp4')
