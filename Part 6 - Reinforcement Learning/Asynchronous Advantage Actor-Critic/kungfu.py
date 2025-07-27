1540ddc249f5b3f7ccfbc616a248b8c6cc063aeb
Cydonia01 <malizdmr011@gmail.com>
2025-07-27 21:46:45 +0300

    implemented kung fu master using A3C algorithm

---
+++ Part 6 - Reinforcement Learning/Asynchronous Advantage Actor-Critic/kungfu.py
@@ -0,0 +1,208 @@
+import cv2
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import gymnasium as gym
+from gymnasium import ObservationWrapper
+from gymnasium.spaces import Box
+import imageio
+import webbrowser
+from ale_py import ALEInterface
+import tqdm
+
+ale = ALEInterface()
+ale.loadROM("kung_fu_master.bin")
+
+# Building the AI
+# Creating the architecture of the neural network
+class Network(nn.Module):
+    def __init__(self, action_size):
+        super(Network, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels = 4, out_channels = 32, kernel_size = (3, 3), stride = 2)
+        self.conv2 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = (3, 3), stride = 2)
+        self.conv3 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = (3, 3), stride = 2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(512, 128)
+        self.fc2a = nn.Linear(128, action_size) # For policy
+        self.fc2s = nn.Linear(128, 1) # For value
+    
+    def forward(self, state):
+        x = F.relu(self.conv1(state))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = self.flatten(x)
+        x = F.relu(self.fc1(x))
+        action_values = self.fc2a(x)
+        state_value = self.fc2s(x).squeeze(-1)
+        return action_values, state_value
+
+# Training the AI
+# Setting up the environment
+class PreprocessAtari(ObservationWrapper):
+
+  def __init__(self, env, height = 42, width = 42, crop = lambda img: img, dim_order = 'pytorch', color = False, n_frames = 4):
+    super(PreprocessAtari, self).__init__(env)
+    self.img_size = (height, width)
+    self.crop = crop
+    self.dim_order = dim_order
+    self.color = color
+    self.frame_stack = n_frames
+    n_channels = 3 * n_frames if color else n_frames
+    obs_shape = {'tensorflow': (height, width, n_channels), 'pytorch': (n_channels, height, width)}[dim_order]
+    self.observation_space = Box(0.0, 1.0, obs_shape)
+    self.frames = np.zeros(obs_shape, dtype = np.float32)
+
+  def reset(self):
+    self.frames = np.zeros_like(self.frames)
+    obs, info = self.env.reset()
+    self.update_buffer(obs)
+    return self.frames, info
+
+  def observation(self, img):
+    img = self.crop(img)
+    img = cv2.resize(img, self.img_size)
+    if not self.color:
+      if len(img.shape) == 3 and img.shape[2] == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = img.astype('float32') / 255.
+    if self.color:
+      self.frames = np.roll(self.frames, shift = -3, axis = 0)
+    else:
+      self.frames = np.roll(self.frames, shift = -1, axis = 0)
+    if self.color:
+      self.frames[-3:] = img
+    else:
+      self.frames[-1] = img
+    return self.frames
+
+  def update_buffer(self, obs):
+    self.frames = self.observation(obs)
+
+def make_env():
+  env = gym.make("KungFuMaster-v4", render_mode = 'rgb_array')
+  env = PreprocessAtari(env, height = 42, width = 42, crop = lambda img: img, dim_order = 'pytorch', color = False, n_frames = 4)
+  return env
+
+env = make_env()
+
+state_shape = env.observation_space.shape
+number_actions = env.action_space.n
+# print("State shape:", state_shape)
+# print("Number actions:", number_actions)
+# print("Action names:", env.unwrapped.get_action_meanings())
+
+# Initializing the hyperparameters
+learning_rate = 1e-4
+discount_factor = 0.99
+number_environments = 10
+
+# Implementing the DQN class
+class Agent():
+    def __init__(self, action_size):
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.action_size = action_size
+        self.network = Network(action_size).to(self.device)
+        self.optimizer = optim.Adam(self.network.parameters(), lr = learning_rate)
+    
+    def act(self, state):
+        if state.ndim == 3:
+            state = [state]
+        state = state = torch.from_numpy(np.array(state)).float().to(self.device)
+        action_values, _ = self.network(state)
+        policy = F.softmax(action_values, dim = -1)
+        return np.array([np.random.choice(len(p), p = p) for p in policy.detach().cpu().numpy()])    
+    
+    def step(self, state, action, reward, next_state, done):
+        batch_size = state.shape[0]
+        state = state = torch.from_numpy(np.array(state)).float().to(self.device)
+        next_state = torch.from_numpy(np.array(next_state)).float().to(self.device)
+        reward = torch.tensor(reward, dtype = torch.float32, device = self.device)
+        done = torch.tensor(done, dtype = torch.bool, device = self.device).to(torch.float32)
+        action_values, state_value = self.network(state)
+        _, next_state_value = self.network(next_state)
+        target_state_value = reward + discount_factor * next_state_value * (1 - done)
+        advantage = target_state_value - state_value
+        probs = F.softmax(action_values, dim = -1)
+        logprobs = F.log_softmax(action_values, dim = -1)
+        entropy = -torch.sum(probs * logprobs, axis = -1)
+        batch_idx = np.arange(batch_size)
+        logp_actions = logprobs[batch_idx, action]
+        actor_loss = -(logp_actions * advantage.detach()).mean() - 0.001 * entropy.mean()
+        critic_loss = F.mse_loss(target_state_value.detach(), state_value)
+        total_loss = actor_loss + critic_loss
+        self.optimizer.zero_grad()
+        total_loss.backward()
+        self.optimizer.step()
+
+# Initializing the agent
+agent = Agent(number_actions)
+
+# Evaluating the agent
+def evaluate(agent, env, n_episodes = 1):
+    episodes_rewards = []
+    for _ in range(n_episodes):
+        state, _ = env.reset()
+        done = False
+        total_reward = 0
+        while not done:
+            action = agent.act(state)
+            state, reward, done, _, _ = env.step(action[0])
+            total_reward += reward
+        episodes_rewards.append(total_reward)
+    return episodes_rewards
+
+# Managing multiple environments simultaneously
+class EnvBatch:
+    def __init__(self, n_envs = 10):
+        self.envs = [make_env() for _ in range(n_envs)]
+    
+    def reset(self):
+        _states = []
+        for env in self.envs:
+            _states.append(env.reset()[0])
+        return np.array(_states)
+    
+    def step(self, actions):
+        next_states, rewards, dones, infos, _ = map(np.array, zip(*[env.step(action) for env, action in zip(self.envs, actions)]))
+        for i in range(len(self.envs)):
+            if dones[i]:
+                next_states[i] = self.envs[i].reset()[0]
+        return next_states, rewards, dones, infos
+    
+# Training the A3C agent
+env_batch = EnvBatch(n_envs = number_environments)
+batch_states = env_batch.reset()
+
+with tqdm.trange(0, 4001) as progress_bar:
+    for i in progress_bar:
+        batch_actions = agent.act(batch_states)
+        batch_next_states, batch_rewards, batch_dones, _ = env_batch.step(batch_actions)
+        batch_rewards *= 0.01
+        agent.step(batch_states, batch_actions, batch_rewards, batch_next_states, batch_dones)
+        batch_states = batch_next_states
+        if i % 1000 == 0:
+            print("Average agent reward: ", np.mean(evaluate(agent, env, n_episodes = 10)))
+
+torch.save(agent.network.state_dict(), 'final_weights.pth')
+
+# agent.network.load_state_dict(torch.load('final_weights.pth', map_location=agent.device))
+    
+# Visualizing the results
+def show_video_of_model(agent, env_name):
+    env = gym.make(env_name, render_mode='rgb_array')
+    env = PreprocessAtari(env, height=42, width=42, crop=lambda img: img, dim_order='pytorch', color=False, n_frames=4)
+    state, _ = env.reset()
+    done = False
+    frames = []
+    while not done:
+        frame = env.render()
+        frames.append(frame)
+        action = agent.act(state)
+        state, _, done, _, _ = env.step(action.item())
+    env.close()
+    imageio.mimsave('video.mp4', frames, fps=30)
+    
+show_video_of_model(agent, 'KungFuMaster-v4')
+webbrowser.open('video.mp4')
